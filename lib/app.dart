fe0f45c16a53144b333ba0d71b3d86ff62db3596
Bibekgupta777 <vivek.gupta.22265@gmai.com>
2024-11-26 10:30:38 +0545

    screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:first_flutter_project/view/first_view.dart';
+import 'package:first_flutter_project/view/airthematic.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FirstView(),
+      home: ArithmeticView(),
     );
   }
 }
